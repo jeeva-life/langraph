11ed16ad81924350a60a975a1c69444f79af3ab7
jeeva-life <kodalijeevan756@gmail.com>
2025-01-10 14:26:15 -0600

    created agent with necessary dependencies and designed graph in neo4j

---
+++ neo4j_chatbot/llm.py
@@ -0,0 +1,15 @@
+# Create the LLM
+from langchain_openai import ChatOpenAI
+import streamlit as st
+
+llm = ChatOpenAI(
+    openai_api_key=st.secrets["OPENAI_API_KEY"],
+    model=st.secrets["OPENAI_MODEL"],
+)
+
+# Create the Embedding model
+from langchain_openai import OpenAIEmbeddings
+
+embeddings = OpenAIEmbeddings(
+    openai_api_key=st.secrets["OPENAI_API_KEY"]
+)
